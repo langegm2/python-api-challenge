8598e21fa7ec04c024d769d2fb93fe105a44cac2
langegm2 <119615284+langegm2@users.noreply.github.com>
2023-02-05 13:58:04 -0500

    update and add files from weatherpy

--- Starter_Code/api_keys.py
+++ Starter_Code/api_keys.py
@@ -1,5 +1,6 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "4879ecd4cd6e9d5d00181c09ecbc88ed"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "03bec0c45b2947c4a79fcd1854e2c296"
+.gitignore
--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "4879ecd4cd6e9d5d00181c09ecbc88ed"
 
 # Geoapify API Key
 geoapify_key = "YOUR KEY HERE"
